7617281f181995af6596ddf94776258c41ba16a0
Stewart Dulaney <sdulaney@users.noreply.github.com>
2019-01-30 13:08:39 -0800

    show the hero object

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,7 +8,10 @@ import { Hero } from '../hero';
 })
 export class HeroesComponent implements OnInit {
 
-  hero = 'Windstorm';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  }
 
   constructor() { }
 
